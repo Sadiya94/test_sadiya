8dd9d434e5c9df286982316414580eff65fff9f6
Sadiya94 <sadiyasultana@Sadiyas-MacBook-Air.local>
2014-07-11 11:56:20 -0500

    added python directory

---
+++ src/python/utils/csv2text.py
@@ -0,0 +1,16 @@
+''' This python program should read each line of essays.csv and save it as a separate file with a the name essay<line_number>.txt
+
+That is, the first line should be saved as essay1.txt, the second line as essay2.txt, etc.
+'''
+hello = 'hi'
+print hello
+
+
+
+ 
+
+
+
+
+
+
