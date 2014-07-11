83c9757a01a7662fe0d4f4d94bc8cbf286bc79ac
Danilo Jara <d-jara@neiu.edu>
2014-07-10 23:27:58 -0500

    Improved your m.py file

--- src/m.py
+++ src/m.py
@@ -2,8 +2,11 @@
 import string
 filename = "essays.txt"
 file_object = open(filename, 'r')
+txtfile = open ("output.txt", "w") # Sadiya this line here of code creates a new file.
+
 count =0
 for line in file_object:
-    x = line.translate(string.maketrans("",""), string.punctuation)
-    print x
+     x = line.translate(string.maketrans("",""), string.punctuation)
+     txtfile.write(x) # In this part we are writing the current into our new document.
+txtfile.close() # In this last part we are actually closing our file
    
